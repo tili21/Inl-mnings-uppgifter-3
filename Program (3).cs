073df8159a7adda5b9a275714f6712733976a6d0
tili21 <tili08001@utb.vaxjo.se>
2025-12-02 12:37:55 +0100

    Add files via upload

---
+++ Program (3).cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? (j/n)");
+            string svar = Console.ReadLine();
+
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (svar == "j" && ålder < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig!");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+            }
+        }
+    }
+}
