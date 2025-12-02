d6f0049c3b046deadcdcb4664e4370d1bff08824
tili21 <tili08001@utb.vaxjo.se>
2025-12-02 12:30:12 +0100

    Add files via upload

---
+++ Program (2).cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Test
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder =int.Parse(Console.ReadLine());
+
+            if (ålder >= 16 && ålder <= 19)
+            {
+                Console.WriteLine("Du får delta i tävlingen!");
+            }
+            else if (ålder < 16)
+            {
+                Console.WriteLine("Du är för ung för att delta.");
+            }
+            else
+            {
+                Console.WriteLine("Du är för gammalför att delta.");
+            }
+
+
+        }
+
+    }
+}  
